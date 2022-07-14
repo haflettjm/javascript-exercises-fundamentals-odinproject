4d8bf8ba69faec337b8506bfd96e0b0b0303c1e1
Jacob Haflett <hafletjm01@gmail.com>
2022-07-13 21:57:52 -0500

    finished 007

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
 
+Number.prototype.round = function(places) {
+  return +(Math.round(this + "e+" + places)  + "e-" + places);
+}
+const ftoc = function(f) {
+  // [°C] = ([°F] − 32) × 5⁄9
+  let conversion = (f  - 32) * 5/9
+  conversion = conversion.round(1)
+  return conversion;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  // [°F] = [°C] × 9⁄5 + 32 
+  let conversion = (c  * 9/5) + 32
+  conversion = conversion.round(1)
+  return conversion;
 };
 
 // Do not edit below this line
