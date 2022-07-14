a475316aef9b9fe1d83de037751d5412c582f436
Jacob Haflett <hafletjm01@gmail.com>
2022-07-13 20:39:18 -0500

    finished exercise 003

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let newString = ''
+    if(input != ''){        
+        for(let i = input.length; i > 0; i--){
+            newString += input[i-1];
+        }
+        return newString;
+    }else{
+        return input;
+    }
 };
 
 // Do not edit below this line
