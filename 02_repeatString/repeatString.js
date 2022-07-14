2d63d64fb713a9d3347a35f766f09f5366f3ac72
Jacob Haflett <hafletjm01@gmail.com>
2022-07-13 20:32:09 -0500

    finished exercise 002

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString ='';
+    if(num >= 0){
+        do{
+            if(num > 0){
+                newString = newString + string;
+                num = num - 1
+            }
+        }while(num > 0);
+        return newString;
+    }else{
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
