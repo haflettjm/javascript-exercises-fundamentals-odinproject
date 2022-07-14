cff7e9cc3edafcb5f33100b214554fa3cf8a8681
Jacob Haflett <hafletjm01@gmail.com>
2022-07-13 21:12:28 -0500

    finished exercise 004

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    for(let x = arguments.length; x > 0; x--){
+        for(let i = 0; i < array.length; i++){
+            if(array[i] === arguments[x]){
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
