f529b0b5abbf937ec2cfb6a6705ac12dc0a2d195
haflettjm <hafletjm01@gmail.com>
2023-02-16 22:02:09 -0600

    completed find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    function getAge(birth, death){
+        if(!death){
+            return 2023 - birth
+        }
+        return death - birth
+    }
 
+
+   let oldest =  people.reduce((oldestPerson, currentPerson)=>{
+        oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+        currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentPersonAge ? currentPerson : oldestPerson;
+   })
+
+   return oldest;
 };
 
 // Do not edit below this line
