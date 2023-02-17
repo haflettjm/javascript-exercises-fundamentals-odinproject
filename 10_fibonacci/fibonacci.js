140de848b04b8741775d0beda6ef2bca88c05bf6
haflettjm <hafletjm01@gmail.com>
2023-02-16 21:14:05 -0600

    completed fiboonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let n1 = 0;
+    let n2 = 1;
+    let next;
+    num = parseInt(num)
+    if(num < 0){
+        return "OOPS"
+    }
+    for(let i = 1; i < num; i++){
+        next = n1 + n2;
+        n1 = n2;
+        n2 = next;
+    }
 
+    return n2
 };
 
 // Do not edit below this line
