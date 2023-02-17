652a8848145bb4474642ab2df1564201de900a65
haflettjm <hafletjm01@gmail.com>
2023-02-16 20:30:10 -0600

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let bool = true;
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    string = string.replace(/\s+/g, '');
+    string = string.toLowerCase();
+    console.log(string);
+    let arr1 = Array.from(string);
+    let arr2 = [...arr1].reverse()
 
+    for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] !== arr2[i]){
+            bool = false;
+        }
+    }
+    
+    return bool
 };
 
 // Do not edit below this line
